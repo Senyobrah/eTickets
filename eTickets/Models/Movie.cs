8410b47406b1547ad8946e4b8180bde028137c30
Enyo <senyobrah@gmail.com>
2024-08-03 23:49:53 +0300

    04.08.2024 model relationships

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using eTickets.Data;
 
 namespace eTickets.Models
@@ -21,9 +22,17 @@ namespace eTickets.Models
 
 		// Relationships
 		public List<Actor_Movie> Actors_Movies { get; set; }
+		public List<Actor_Movie> Actor_Movies { get; set; }
 
 		//Cinema
+		public int CinemaId { get; set; }
+		[ForeignKey("CinemaId")]
 		public Cinema Cinema { get; set; }
+
+		//Producer
+		public int ProducerId { get; set; }
+		[ForeignKey("ProducerId")]
+		public Producer Producer { get; set; }
 
 	}
 }
